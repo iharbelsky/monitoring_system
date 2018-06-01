9ca9a7385a850fc159a201ea5081c61200ece538
igor.belsky <igor.belsky@vrpinc.com>
2018-06-01 15:59:19 +0300

    generated version with updated dependencies

---
+++ src/main/java/vrp/MonitoringApplication.java
@@ -0,0 +1,16 @@
+package vrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class MonitoringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MonitoringApplication.class, args);
+		var t = new HashMap<>();
+	}
+}
---
+++ src/test/java/vrp/MonitoringApplicationTests.java
@@ -0,0 +1,20 @@
+package vrp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonitoringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		var t = new HashMap<>();
+
+	}
+
+}
