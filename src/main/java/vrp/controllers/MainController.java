def54b13c6c1a85f6bbc9987118f0c5643ec1d42
bondarchukk <kbandarchyk@gmail.com>
2018-06-01 17:49:39 +0300

    Added security integration

---
+++ src/main/java/vrp/controllers/MainController.java
@@ -0,0 +1,25 @@
+package vrp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value={"/","/home"},method = RequestMethod.GET)
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping(value="/login",method =RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+
+}
