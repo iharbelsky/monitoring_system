40b02f9c8eadacd02444bd7387c97738cf89bbc3
bondarchukk <kbandarchyk@gmail.com>
2018-06-05 18:31:27 +0300

    1.Added next service-interfaces(ProjectService,ModuleService,LogService) 2.Implement ProjectServiceImpl 3. Refactored ProjectController

--- src/main/java/vrp/controllers/ProjectController.java
+++ src/main/java/vrp/controllers/ProjectController.java
@@ -1,24 +1,31 @@
 package vrp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import vrp.domain.Project;
-import vrp.repositories.ProjectRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import vrp.dto.ProjectDTO;
+import vrp.service.ProjectService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/project")
 public class ProjectController {
 
+
     @Autowired
-    private ProjectRepository projectRepo;
+    private  ProjectService projectService;
+
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public Iterable<Project> getAllProjects() {
-        return projectRepo.findAll();
+    public List<ProjectDTO> getAllProjects() {
+        return projectService.getAllProjects();
     }
 
     @RequestMapping(value = "/set", method = RequestMethod.POST)
-    public Project setProject(@RequestBody Project project) {
-        return projectRepo.save(project);
+    public void setProject(@RequestBody ProjectDTO projectDTO) {
+         projectService.saveProject(projectDTO);
     }
 }
--- src/main/java/vrp/repositories/ProjectRepository.java
+++ src/main/java/vrp/repositories/ProjectRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vrp.domain.Project;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,Long> {
 
-
+    Optional<Project> findByNameProject(String nameProject);
 }
