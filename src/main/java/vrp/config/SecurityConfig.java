3b1235908c02de0c218ba319bb0b7d60b128da1c
bondarchukk <kbandarchyk@gmail.com>
2018-06-07 11:39:51 +0300

    1.Refactoring class SecurityConfig

--- src/main/java/vrp/config/SecurityConfig.java
+++ src/main/java/vrp/config/SecurityConfig.java
@@ -14,16 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/home")
-                .permitAll()
-                .antMatchers("/**")
-                .hasRole("USER")
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .usernameParameter("username")
-                .passwordParameter("password");
+            .antMatchers("/", "/home")
+            .permitAll()
+            .antMatchers("/**")
+            .hasRole("USER")
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .usernameParameter("username")
+            .passwordParameter("password");
     }
 }
 
